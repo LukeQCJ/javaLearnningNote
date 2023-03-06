be6d922b5babd4ed7578ab6636cb183c34fc3515
LukeQCJ <853223579@qq.com>
2023-03-06 16:47:49 +0800

    连续数列代码,添加注释

--- src/main/com/luke/Problem4.java
+++ src/main/com/luke/Problem4.java
@@ -1,20 +1,5 @@
-## 第4题：求解连续数列
-给定一个整数数组，找出总和最大的连续数列，并返回总和。
+package com.luke;
 
-示例：
-
-输入： [-2,1,-3,4,-1,2,1,-5,4]
-输出： 6
-解释： 连续子数组 [4,-1,2,1] 的和最大，为 6。
-进阶：
-
-如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的分治法求解。
-
-来源：力扣（LeetCode）
-链接：https://leetcode.cn/problems/contiguous-sequence-lcci
-
-代码：
-```
 import java.util.Scanner;
 
 public class Problem4 {
@@ -47,4 +32,3 @@ public class Problem4 {
         return maxAns;
     }
 }
-```
