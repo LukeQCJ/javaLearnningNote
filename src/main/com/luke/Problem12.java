90667aefb6966b348bfb5c3bd7f0061a96f4ae8c
LukeQCJ <853223579@qq.com>
2023-03-08 16:52:52 +0800

    最长子串长度代码,添加注释

--- src/main/com/luke/Problem12.java
+++ src/main/com/luke/Problem12.java
@@ -1,35 +1,5 @@
-## 无重复字符的最长子串
-给定一个字符串 s ，请你找出其中不含有重复字符的最长子串的长度。
+package com.luke;
 
-
-示例1:
-
-输入: s = "abcabcbb"
-输出: 3
-解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
-示例 2:
-
-输入: s = "bbbbb"
-输出: 1
-解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
-示例 3:
-
-输入: s = "pwwkew"
-输出: 3
-解释: 因为无重复字符的最长子串是"wke"，所以其长度为 3。
-    请注意，你的答案必须是 子串 的长度，"pwke"是一个子序列，不是子串。
-
-
-提示：
-
-0 <= s.length <= 5 * 104
-s由英文字母、数字、符号和空格组成
-
-来源：力扣（LeetCode）
-链接：https://leetcode.cn/problems/longest-substring-without-repeating-characters
-
-代码：
-```
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -59,4 +29,3 @@ public class Problem12 {
         return maxLen;
     }
 }
-```
