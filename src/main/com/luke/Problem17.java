660b35426c3baac720971c5ea9c1ba3575b01419
LukeQCJ <853223579@qq.com>
2023-03-09 15:26:43 +0800

    亲自游戏代码,添加注释

--- src/main/com/luke/Problem17.java
+++ src/main/com/luke/Problem17.java
@@ -1,40 +1,5 @@
-## 亲子游戏
+package com.luke;
 
-题目描述：
-幼儿园老师安排小朋友做游戏，现在需要给N个小朋友进行分组。
-老师让每个同学写一个名字，代表这位小朋友想和谁分到一组，请问老师在满足所有小朋友意愿的情况下，最多可以将班级分成多少组？
-
-输入描述：
-第一行输入N，0<N<100000
-接下来是N行代表每个小朋友希望和谁分到一组，
-如“John Jack”，代表John希望和Jack分到一组，两个名字之间以空格分隔，名字本身不存在空格。
-
-输出描述：
-分组的最多数量
-
-示例1：
-输入
-6
-Jack Tom
-Alice John
-Jessica Leonie
-Tom Alice
-John Jack
-Leonie Jessica
-输出
-2
-
-示例2：
-输入
-3
-Xiaoming Xiaohong
-Xiaohong Xiaoqiang
-Xiaoqiang Xiaoming
-输出
-1
-
-代码:
-```
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -73,4 +38,3 @@ public class Problem17 {
         }
     }
 }
-```
