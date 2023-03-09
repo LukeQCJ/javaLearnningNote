6e7e9e768f08d2448403678287cd98336b5f6e90
LukeQCJ <853223579@qq.com>
2023-03-09 10:33:33 +0800

    最大数代码,添加注释

--- src/main/com/luke/Problem14.java
+++ src/main/com/luke/Problem14.java
@@ -1,26 +1,5 @@
-## 最大数
-给定一组非负整数 nums，重新排列每个数的顺序（每个数不可拆分）使之组成一个最大的整数。
+package com.luke;
 
-注意：输出结果可能非常大，所以你需要返回一个字符串而不是整数。
-
-示例 1：
-输入：nums = [10,2]
-输出："210"
-
-示例2：
-输入：nums = [3,30,34,5,9]
-输出："9534330"
-
-提示：
-
-1 <= nums.length <= 100
-0 <= nums[i] <= 109
-
-来源：力扣（LeetCode）
-链接：https://leetcode.cn/problems/largest-number
-
-代码:
-```
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -56,4 +35,3 @@ public class Problem14 {
         return "".equals(sb.toString()) ? "0" : sb.toString();
     }
 }
-```
