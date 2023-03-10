8a872e92153af9004f710a71004d44ca058cd6af
LukeQCJ <853223579@qq.com>
2023-03-10 14:31:43 +0800

    字符串分割问题 代码,添加注释

---
+++ src/main/com/luke/Problem27.java
@@ -0,0 +1,27 @@
+package com.luke;
+
+import java.util.Scanner;
+
+public class Problem27 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        splitString(s);
+        sc.close();
+    }
+
+    public static void splitString(String s) {
+        if (s == null || s.length() == 0) {
+            return;
+        }
+        StringBuilder temp = new StringBuilder(s);
+        while (temp.length() > 8) {
+            System.out.println(temp.substring(0,8));
+            temp = new StringBuilder(temp.substring(8));
+        }
+        while (temp.length() < 8 && temp.length() > 0) {
+            temp.append("0");
+        }
+        System.out.println(temp.toString());
+    }
+}
