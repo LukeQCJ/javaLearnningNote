c2d7bb1c7209fae3f2d588b9eb83856686ff181c
Luke <853223579@qq.com>
2023-03-11 16:20:51 +0800

    排序序列问题 代码

--- src/main/com/luke/Problem31.java
+++ src/main/com/luke/Problem31.java
@@ -1,40 +1,5 @@
-## 排列序列
+package src.main.com.luke;
 
-给出集合[1,2,3,...,n]，其所有元素共有n! 种排列。
-
-按大小顺序列出所有排列情况，并一一标记，当n = 3 时, 所有排列如下：
-
-"123"
-"132"
-"213"
-"231"
-"312"
-"321"
-给定n 和k，返回第k个排列。
-
-示例 1：
-
-输入：n = 3, k = 3
-输出："213"
-示例 2：
-
-输入：n = 4, k = 9
-输出："2314"
-示例 3：
-
-输入：n = 3, k = 1
-输出："123"
-
-
-提示：
-
-1 <= n <= 9
-1 <= k <= n!
-
-来源：力扣（LeetCode）
-链接：https://leetcode.cn/problems/permutation-sequence
-
-```
 import java.util.*;
 
 public class Problem31 {
@@ -80,5 +45,3 @@ public class Problem31 {
         return false;
     }
 }
-```
-
