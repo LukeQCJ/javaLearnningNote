8da84114435bf2dbe9b8f6d4dffc322400a27305
LukeQCJ <853223579@qq.com>
2023-03-20 16:50:51 +0800

    亲子游戏 注释

--- src/main/com/luke/Problem17.java
+++ src/main/com/luke/Problem17.java
@@ -21,20 +21,24 @@ public class Problem17 {
                 intentionMap.put(relation[0], relation[1]);
                 childrenArray[i] = relation[0]; // 只保存第一个同学
             }
-            // 小朋友集合,用于判断是否已经处理过
-            HashSet<String> childrenSet = new HashSet<>();
-            int groupCount = 0;
-            for (int i = 0; i < n; i++) { // 遍历每个小朋友，查看每个小朋友的意向
-                if (!childrenSet.contains(childrenArray[i])){
-                    String child = childrenArray[i];
-                    while (!childrenSet.contains(child)){
-                        childrenSet.add(child);
-                        child = intentionMap.get(child); // 通过意向小朋友迭代找同组的下一个小朋友
-                    }
-                    groupCount++;
-                }
-            }
-            System.out.println(groupCount);
+            System.out.println(computeGroup(n, intentionMap, childrenArray));
         }
+    }
+
+    private static int computeGroup(int n, Map<String, String> intentionMap, String[] childrenArray) {
+        // 小朋友集合,用于判断是否已经处理过
+        HashSet<String> childrenSet = new HashSet<>();
+        int groupCount = 0;
+        for (int i = 0; i < n; i++) { // 遍历每个小朋友，查看每个小朋友的意向
+            if (!childrenSet.contains(childrenArray[i])){
+                String child = childrenArray[i];
+                while (!childrenSet.contains(child)){
+                    childrenSet.add(child);
+                    child = intentionMap.get(child); // 通过意向小朋友迭代找同组的下一个小朋友
+                }
+                groupCount++;
+            }
+        }
+        return groupCount;
     }
 }
