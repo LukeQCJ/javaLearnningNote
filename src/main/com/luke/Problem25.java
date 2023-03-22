4d5dd06f3bb7910afa4db29bd83551f585dc7b03
Luke <853223579@qq.com>
2023-03-22 22:46:48 +0800

    简易内存池分配问题 代码和注释

--- src/main/com/luke/Problem25.java
+++ src/main/com/luke/Problem25.java
@@ -8,25 +8,26 @@ import java.util.TreeMap;
 public class Problem25 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        String[] sizeList = sc.nextLine().split(",");
+        String[] resList = sc.nextLine().split(",");
         String[] requestList = sc.nextLine().split(",");
-        //用treeMap将大小和数量对应起来，并按大小排序
-        TreeMap<Integer, Integer> map = new TreeMap<>();
-        for(String s : sizeList){
+        // 用treeMap将大小和数量对应起来，并按size顺序排序
+        TreeMap<Integer, Integer> resourceMap = new TreeMap<>();
+        for(String s : resList){
             Integer size = Integer.parseInt(s.split(":")[0]);
             Integer num = Integer.parseInt(s.split(":")[1]);
-            map.put(size, num);
+            resourceMap.put(size, num);
         }
 
         StringBuilder sb = new StringBuilder();
-        List<Integer> resourceList = new ArrayList<>(map.keySet()); //有参构造！！
+        List<Integer> resourceList = new ArrayList<>(resourceMap.keySet()); //有参构造！！
         for(String reqStr : requestList){ // 遍历 请求list
             int flag = 0;
             for(int resource : resourceList){ // 遍历资源list
                 int req = Integer.parseInt(reqStr);
-                if(req <= resource && map.get(resource) > 0){ // 如果有合适的资源，则申请成功
+                // 如果有合适的资源，则申请成功
+                if(req <= resource && resourceMap.get(resource) > 0){
                     sb.append("true").append(",");
-                    map.put(resource, map.get(resource) - 1); //更新内存池
+                    resourceMap.put(resource, resourceMap.get(resource) - 1); //更新内存池
                     flag = 1;
                     break;
                 }
