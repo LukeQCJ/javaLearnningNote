55c6e6de3aaca0a05d1db8b99b60b0bc2a98ee17
LukeQCJ <853223579@qq.com>
2023-03-24 15:01:56 +0800

    字符串解码 的代码和注释

--- src/main/com/luke/Problem26.java
+++ src/main/com/luke/Problem26.java
@@ -1,6 +1,5 @@
 package com.luke;
 
-import java.util.Objects;
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -14,24 +13,25 @@ public class Problem26 {
 
     public static String decodeString(String s) {
         // 1、迭代的退出条件
-        if (Objects.isNull(s) || s.length() == 0) {
+        if (s == null || s.length() == 0) {
             return "";
         }
         int len = s.length();
         // 2、解码流程开始
         Stack<Integer> numStack = new Stack<>(); // 保存字符串的重复次数
         Stack<String> strStack = new Stack<>();  // 保存同一层级的字符串
-        StringBuilder numStr = new StringBuilder();
-        StringBuilder tempStr = new StringBuilder();
+        StringBuilder tempStr = new StringBuilder(); // 临时变量,用于记录过程字符串
+        StringBuilder numStr = new StringBuilder();  // 用于记录过程字符串 重复的次数
         int squareBracketLevel = 0; // 中括号的层数
         for (int i = 0; i < len; i++) {
             char c = s.charAt(i);
+            // 1) 处理数字字符
             if (Character.isDigit(c)) {
                 if (squareBracketLevel > 0) { // 如果数字包含在中括号里面，则拼接进tempStr待内层解析
                     tempStr.append(c);
                     continue;
                 }
-                // 头部是以字母序列开头的,即类似adc2[ab]xx
+                // 处理以字母序列开头的子串,如adc2[ab]xx
                 if ("".equals(numStr.toString()) && !"".equals(tempStr.toString())) {
                     numStr.append("1");
                     numStack.add(Integer.parseInt(numStr.toString()));
@@ -40,11 +40,13 @@ public class Problem26 {
                     numStr = new StringBuilder();
                 }
                 numStr.append(c);
+            // 2) 处理左方括号[字符
             } else if (c == '[') {
                 if (squareBracketLevel > 0) { // 不是第一层括号,则直接追加到字符串中，待下一层处理
                     tempStr.append(c);
                 }
                 squareBracketLevel++; // 括号层数+1
+            // 3) 处理右方括号]字符
             } else if (c == ']') {
                 squareBracketLevel--;
                 if (squareBracketLevel <= 0) { // 如果是第一层括号，则将数字、字符串放入 栈 中，以待后续处理
@@ -55,20 +57,23 @@ public class Problem26 {
                     strStack.add(tempStr.toString());
                     tempStr = new StringBuilder();
                     numStr = new StringBuilder();
-                } else { // 不是第一层括号,则直接追加到字符串中，待下一层处理
+                } else { // 不是第一层括号,则直接追加到字符串中，待下一层去递归处理
                     tempStr.append(c);
                 }
+            // 4) 处理字母字符
             } else if (Character.isLetter(c)) {
                 tempStr.append(c);
+                // 处理以字母序列结尾的子串,如abc2[a]xxx
+                if (i == len - 1) {
+                    if ("".equals(numStr.toString())) {
+                        numStr.append("1");
+                    }
+                    numStack.add(Integer.parseInt(numStr.toString()));
+                    strStack.add(tempStr.toString());
+                }
             }
         }
-        // 处理以字母结尾的例子: abc2[a]xxx
-        if ("".equals(numStr.toString())) {
-            numStr.append("1");
-        }
-        numStack.add(Integer.parseInt(numStr.toString()));
-        strStack.add(tempStr.toString());
-        // 开始借助strStack和numStack凭借字符串
+        // 3、开始借助strStack和numStack拼接字符串
         int size = numStack.size();
         StringBuilder result = new StringBuilder();
         for (int i = 0; i < size; i++) {
@@ -76,7 +81,7 @@ public class Problem26 {
             String temp = s1;
             // 如果是待解析的字符串就解析
             if (s1.contains("[")) {
-                temp = decodeString(s1);
+                temp = decodeString(s1); // 递归解析
             }
             Integer num = numStack.get(i);
             for (int j = 0; j < num; j++) {
