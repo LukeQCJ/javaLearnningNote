347026302a3b9b15c0536d19b66e57f5e766b089
LukeQCJ <853223579@qq.com>
2023-03-24 17:09:46 +0800

    BOSS的收入 的代码和注释

--- src/main/com/luke/Problem28.java
+++ src/main/com/luke/Problem28.java
@@ -5,31 +5,37 @@ import java.util.Scanner;
 public class Problem28 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int time = sc.nextInt();
-        int count = sc.nextInt();
+        int time = sc.nextInt(); // 工作最多时长
+        int count = sc.nextInt(); // 可供选择的工作个数,即工作清单的工作条数
         // 初始化工作清单和报酬清单
-        int[] times = new int[count];
+        int[] costTimes = new int[count];
         int[] rewards = new int[count];
         for (int i = 0; i < count; i++) {
             int workTime = sc.nextInt();
             int reward = sc.nextInt();
-            times[i] = workTime;
+            costTimes[i] = workTime;
             rewards[i] = reward;
         }
-        // dp[i][j] 表示在选择第i份工作的时候在j时间内小明能赚到的最多工资
-        int[][] dp = new int[count + 1][time + 1];
+        int maxReward = getMaxReward(time, count, costTimes, rewards);
+        System.out.println(maxReward);
+        sc.close();
+    }
+
+    private static int getMaxReward(int longestWorkTime, int count, int[] costTimes, int[] rewards) {
+        // dp[workNum][curTime] 表示在选择第workNum份工作的时候在curTime时间内小明能赚到的最多工资
+        int[][] dp = new int[count + 1][longestWorkTime + 1];
         dp[0][0] = 0; // 表示在没有选择工作，没有花时间的工资
-        for (int i = 1; i <= count; i++) {
-            for (int j = 0; j <= time; j++) {
-                // 表示在第i - 1份工作j时间内的报酬与第i份工作在j时间内的工资报酬是一样的，因为这个时候第i份工作还没有开始
+        for (int workNum = 1; workNum <= count; workNum++) {
+            for (int curTime = 0; curTime <= longestWorkTime; curTime++) {
+                // 表示在第workNum - 1份工作curTime时间内的报酬与第workNum份工作在curTime时间内的工资报酬是一样的，因为这个时候第workNum份工作还没有开始
                 // 即 前一份工作结束的报酬和后一份工作开始的报酬是一样的
-                dp[i][j] = dp[i - 1][j];
-                if (j - times[i - 1] >= 0) {
-                    dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - times[i - 1]] + rewards[i - 1]);
+                dp[workNum][curTime] = dp[workNum - 1][curTime];
+                if (curTime - costTimes[workNum - 1] >= 0) {
+                    dp[workNum][curTime] = Math.max(dp[workNum][curTime],
+                            dp[workNum - 1][curTime - costTimes[workNum - 1]] + rewards[workNum - 1]);
                 }
             }
         }
-        System.out.println(dp[count][time]);
-        sc.close();
+        return dp[count][longestWorkTime];
     }
 }
