5d605da988a19c4e9ff197eecbf5e3d397b913e3
Luke <853223579@qq.com>
2023-03-26 14:01:07 +0800

    字符统计与重排 代码和注释

--- src/main/com/luke/Problem33.java
+++ src/main/com/luke/Problem33.java
@@ -11,9 +11,6 @@ public class Problem33 {
     }
 
     public static String statisticsChar(String s) {
-        Queue<CharStatisticsInfo> queue = new PriorityQueue<>(
-                (o1,o2) -> o1.c == o2.c ? o2.count - o1.count : o1.c - o2.c
-        );
         Map<Character,Integer> charMap = new HashMap<>();
         for (int i = 0; i < s.length(); i++) {
             if (charMap.containsKey(s.charAt(i))) {
@@ -23,26 +20,30 @@ public class Problem33 {
                 charMap.put(s.charAt(i), 1);
             }
         }
-        for (Map.Entry<Character,Integer> entry : charMap.entrySet()) {
-            Character key = entry.getKey();
-            Integer value = entry.getValue();
-            queue.add(new CharStatisticsInfo(key,value));
-        }
+        Queue<Map.Entry<Character, Integer>> queue = new PriorityQueue<>(
+                (o1, o2) -> {
+                    if (o2.getValue() - o1.getValue() != 0) {
+                        return o2.getValue() - o1.getValue();
+                    }
+                    Character c1 = o1.getKey();
+                    Character c2 = o2.getKey();
+                    boolean isUpperC1 = c1 >= 'A' && c1 <= 'Z';
+                    boolean isUpperC2 = c2 >= 'A' && c2 <= 'Z';
+                    if ((isUpperC1 && isUpperC2) || (!isUpperC1 && !isUpperC2)) {
+                        return c1 - c2;
+                    }
+                    if (isUpperC1) {
+                        return 1;
+                    }
+                    return -1;
+                }
+        );
+        queue.addAll(charMap.entrySet());
         StringBuilder result = new StringBuilder();
         while (!queue.isEmpty()) {
-            CharStatisticsInfo charStatisticsInfo = queue.poll();
-            result.append(charStatisticsInfo.c).append(":").append(charStatisticsInfo.count).append(";");
+            Map.Entry<Character,Integer> entry = queue.poll();
+            result.append(entry.getKey()).append(":").append(entry.getValue()).append(";");
         }
         return result.toString();
-    }
-
-    static class CharStatisticsInfo {
-        char c;
-        int count;
-
-        CharStatisticsInfo(char c, int count) {
-            this.c = c;
-            this.count = count;
-        }
     }
 }
