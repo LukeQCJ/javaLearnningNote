1faf2383d9b2f6e82b5f717f287a241acfa0aa74
LukeQCJ <853223579@qq.com>
2023-03-27 14:20:03 +0800

    服务失效判断 的代码和注释

--- src/main/com/luke/Problem11.java
+++ src/main/com/luke/Problem11.java
@@ -26,13 +26,16 @@ public class Problem11 {
         for (String serviceRelationStr : serviceRelations) {
             String[] services = serviceRelationStr.split("-");
             Queue<String> availableServiceQueue = new LinkedList<>();
+            // 按照服务依赖的先后顺序放入队列
             for (String service : services) {
+                // 1、如果发现当前服务已经故障，这当前队列中的服务都已故障，则清空队列
                 if (brokenServicesSet.contains(service)) {
                     availableServiceQueue.clear();
-                } else {
-                    availableServiceQueue.add(service);
+                    continue;
                 }
+                availableServiceQueue.add(service);
             }
+            // 剩下的有效服务在队列中，添加到可用服务结果availableService
             while (!availableServiceQueue.isEmpty()) {
                 availableService.append(availableServiceQueue.poll()).append(",");
             }
