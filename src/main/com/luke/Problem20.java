dd315b9a940709886f17443d3e3e187bc4d13048
LukeQCJ <853223579@qq.com>
2023-03-27 17:25:54 +0800

    Lisp语法解析 的代码和注释

--- src/main/com/luke/Problem20.java
+++ src/main/com/luke/Problem20.java
@@ -19,11 +19,14 @@ public class Problem20 {
     }
 
     private static int cal(String expressionStr, Map<String, Integer> preVariableScope) {
+        // 一、解析 命令表达式字符串
         String[] instructions = splitExpression(expressionStr);
+        // 二、计算命令表达式
         String command = instructions[0]; // 命令 let add mult
         int commandLength = instructions.length;
-        if (LET.equals(command)) { // LET命令 let表达式格式: let v1 e1 ..... vn en expr
-            // 1、先处理变量对的赋值: i是变量,i+1是变量i的值
+        // 二、计算命令表达式: 1、LET命令 let表达式格式: let v1 e1 ..... vn en expr
+        if (LET.equals(command)) {
+            // 1)、参数解析: 先处理变量对的赋值: i是变量,i+1是变量i的值
             Map<String, Integer> curVariableScope = new HashMap<>(preVariableScope);
             for (int i = 1; i < commandLength - 2; i += 2) {
                 String vi = instructions[i]; // vi
@@ -43,7 +46,7 @@ public class Problem20 {
                 }
                 curVariableScope.put(vi, eiValue);
             }
-            // 2、处理let的最后一个表达式expr
+            // 2)、计算最终值: 处理let的最后一个表达式expr
             String endExpression = instructions[commandLength - 1];
             if (endExpression.charAt(0) == '(') { // 表达式
                 // 递归 计算表达式
@@ -53,9 +56,10 @@ public class Problem20 {
             } else { // 数字
                 return Integer.parseInt(endExpression);
             }
-        } else { // ADD命令 add表达式: add e1 e2; MULT命令: mult表达式: mult e1 e2
+        // 二、计算命令表达式: 2、ADD或MULT命令 ADD命令 add表达式: add e1 e2; MULT命令: mult表达式: mult e1 e2
+        } else {
             int[] variables = new int[2];
-            // 1、计算 e1 和 e2
+            // 1)、参数解析: 计算 e1 和 e2
             for (int i = 1; i < commandLength; i++) {
                 String ei = instructions[i];
                 if (ei.charAt(0) == '(') { // 判断ei是否为表达式
@@ -67,7 +71,7 @@ public class Problem20 {
                     variables[i - 1] = Integer.parseInt(ei);
                 }
             }
-            // 2、计算最终值
+            // 2)、计算最终值
             if (ADD.equals(command)) { // 加法
                 return variables[0] + variables[1];
             } else if (MUlT.equals(command)) { // 乘法
@@ -85,15 +89,17 @@ public class Problem20 {
      */
     private static String[] splitExpression(String expression) {
         List<String> expressions = new ArrayList<>();
-        // 去掉两个括号
+        // 1、去掉两个括号
         expression = expression.substring(1, expression.length() - 1);
         int left = 0, right = 0;
         while (right < expression.length()) {
             char ch = expression.charAt(right);
-            if (ch == ' ') { // 空格 则分割表达式的命令格式
+            // 2、空格 则分割表达式的命令格式
+            if (ch == ' ') {
                 expressions.add(expression.substring(left, right));
                 left = right + 1;
-            } else if (ch == '(') { // 括号则表示表达式,需要在下一阶段计算时候解析 即递归处理
+            // 3、括号 则表示表达式,需要在下一阶段计算时候解析 即递归处理
+            } else if (ch == '(') {
                 int count = 0;
                 while (right < expression.length()) {
                     // 进行括号匹配
@@ -113,6 +119,7 @@ public class Problem20 {
             }
             right++;
         }
+        // 4、如果表达式expression不是以括号结尾
         if (left < expression.length()) {
             expressions.add(expression.substring(left));
         }
