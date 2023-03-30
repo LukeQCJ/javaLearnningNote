6c16b8e2201a73431c655a144874a0d75ae4d073
LukeQCJ <853223579@qq.com>
2023-03-30 18:09:22 +0800

    ceshi 的代码和注释

--- src/main/com/luke/Problem1.java
+++ src/main/com/luke/Problem1.java
@@ -1,7 +1,6 @@
 package com.luke;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class Problem1 {
     public static void main1(String[] args) {
@@ -23,5 +22,50 @@ public class Problem1 {
         // 最少数量的类型的卡片数，就是该团队最多能凑齐的福卡套数
         System.out.println(counts[0]);
         sc.close();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String t = sc.nextLine();
+        System.out.println(minWindow(s,t));
+        sc.close();
+    }
+
+    public static String minWindow(String s, String t) {
+        if (s == null || s.length() == 0 || t == null || t.length() == 0) {
+            return "";
+        }
+        int sLen = s.length();
+        int tLen = t.length();
+        Map<Character, Integer> charMap = new HashMap<>();
+        int left = 0, right = 0;
+        int minLen = Integer.MAX_VALUE;
+        String minStr = "";
+        while (right < sLen) {
+            char rightChar = s.charAt(right);
+            if (t.contains(rightChar + "")) {
+                charMap.put(rightChar, charMap.getOrDefault(rightChar, 0) + 1);
+            }
+            right++;
+            boolean moveLeft = false;
+            while (charMap.size() >= tLen) {
+                char leftChar = s.charAt(left);
+                if (charMap.containsKey(leftChar)) {
+                    charMap.put(leftChar, charMap.get(leftChar) - 1);
+                    if (charMap.get(leftChar) == 0) {
+                        moveLeft = true;
+                        minLen = Math.min(minLen, right - left + 1);
+                        minStr = s.substring(left, right);
+                        charMap.remove(leftChar);
+                    }
+                }
+                left++;
+            }
+            while (moveLeft && !t.contains(s.charAt(left) + "")) {
+                left++;
+            }
+        }
+        return minLen == Integer.MAX_VALUE ? "" : minStr;
     }
 }
