8ada2ea3cd5b5fd3dc999cb9c9ee374053cede47
LukeQCJ <853223579@qq.com>
2023-03-30 10:54:02 +0800

    test commit command

--- src/main/com/luke/Problem1.java
+++ src/main/com/luke/Problem1.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Problem1 {
-    public static void main(String[] args) {
+    public static void main1(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         // 所有人员的卡片 字符串
@@ -23,5 +23,39 @@ public class Problem1 {
         // 最少数量的类型的卡片数，就是该团队最多能凑齐的福卡套数
         System.out.println(counts[0]);
         sc.close();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        String[] arr = input.split(" ");
+        int[] nums = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            nums[i] = Integer.parseInt(arr[i]);
+        }
+        int target = sc.nextInt();
+        System.out.println(searchInsert(nums,target));
+    }
+
+    public static int searchInsert(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + ((right - left) >> 1);
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return left + 1;
+    }
+
+    public static void test () {
+
     }
 }
