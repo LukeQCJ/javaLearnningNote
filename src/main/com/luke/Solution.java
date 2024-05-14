ed55cc39c94ae91e59e3876a98c33978f18ca599
LukeQCJ <853223579@qq.com>
2024-05-14 10:37:38 +0800

    华为OD算法题：数字排列

--- src/main/com/luke/Solution.java
+++ src/main/com/luke/Solution.java
@@ -4,7 +4,4 @@ class Solution {
     public static void main(String[] args) {
 
     }
-    // 数组 left,right
-    // right的前缀和，preSum[right + 1] = preSum[right] + nums[right]
-    // left-1的前缀和，preSum[left] = preSum[left-1] + nums[left-1]
 }
